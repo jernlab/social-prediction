fb1e18d4ae60c5f75d0c2d009c13f259ddface85
Stephen Payne <koriasutibun@gmail.com>
2021-12-03 00:28:44 -0500

    Saved copy of rater analysis script for experiement pilot. Created modified script that works for real experiment (only uses 1 social context for podcasts)

---
+++ FA21-22/Results/Rater/rater-data-analysis-real.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+tidy <- read.csv("rater-tidy-pilot-11-17-21.csv")
+stories <- c('cake','movie','podcast')
+story <- stories[3]
+
+storyOnly <- tidy %>% filter(Story == story)
+
+print(paste("Story: ", story))
+print("=======================")
+print("=======================")
+
+
+
+  for (i in c(1:5)) {
+    context1ratings <- storyOnly %>% filter(Level == i) %>% filter(Context == 1)
+    context2ratings <- storyOnly %>% filter(Level == i) %>% filter(Context == 2)
+    
+    context1ratings <- context1ratings$Rating
+    context2ratings <- context2ratings$Rating
+    
+    print(paste("Level ", i))
+    print(paste("Context 1 Average: ", mean(context1ratings)))
+    print(paste("Context 2 Average: ", mean(context2ratings)))
+    print("===========================")
+    #print(t.test(context1ratings, context2ratings))
+  }
